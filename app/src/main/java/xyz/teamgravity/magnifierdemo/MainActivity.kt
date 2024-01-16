fda7254394ff9be5507cc089322727315bff229d
raheemadamboev <vividricemaryandmax@gmail.com>
2024-01-16 18:18:26 +0500

    Implemented Magnifier;

--- app/src/main/java/xyz/teamgravity/magnifierdemo/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/magnifierdemo/MainActivity.kt
@@ -3,16 +3,29 @@ package xyz.teamgravity.magnifierdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.MagnifierStyle
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.magnifier
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import xyz.teamgravity.magnifierdemo.ui.theme.MagnifierDemoTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,6 +34,38 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    var offset by remember { mutableStateOf(Offset.Unspecified) }
+
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .pointerInput(Unit) {
+                                detectDragGestures { change, _ ->
+                                    offset = change.position
+                                }
+                            }
+                            .magnifier(
+                                sourceCenter = {
+                                    offset
+                                },
+                                magnifierCenter = {
+                                    offset - Offset(0F, 200F)
+                                },
+                                style = MagnifierStyle(
+                                    size = DpSize(
+                                        width = 100.dp,
+                                        height = 100.dp
+                                    ),
+                                    cornerRadius = 100.dp
+                                )
+                            )
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.tupac),
+                            contentDescription = null
+                        )
+                    }
                 }
             }
         }
